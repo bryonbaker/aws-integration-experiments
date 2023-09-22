de10b6394fd319cbc5a097a098844cfa830b7b6b
Bryon Baker <bryonbakeraus@gmail.com>
2023-09-22 07:05:01 +0000

    Added import test

--- python/main.py
+++ python/main.py
@@ -1,10 +1,16 @@
 import test_module as fm
+import time
 
 def main():
     print("Running  test")
     pi = 3.141592654
 
     print("Calling print_val()")
+    
+    t = 5
+    print(f"Going to sleep for {t} minutes")
+    time.sleep(t*60)
+    
     
     fm.print_val(pi)
     
